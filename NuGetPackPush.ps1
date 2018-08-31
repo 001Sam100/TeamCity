70cdbcf28d891533d4edaf34836064b99f84d500
Shailesh Singh Rawat <shailesh.1920@gmail.com>
2018-09-01 00:15:48 +0530

    Update NuGetPackPush.ps1

--- NuGetPackPush.ps1
+++ NuGetPackPush.ps1
@@ -1,4 +1,4 @@
 ﻿cd F:\NuGet
 F:
-.\nuget.exe pack -Version $build.number TeamDemo.nuspec
-.\nuget.exe push F:\NuGet\TeamCity.$build.number.nupkg -Source "http://localhost/nuget/packages" -ApiKey API-8KXKBKEZQPTTNJDMQYUAWWBB0VQ
+.\nuget.exe pack -Version $build.number$ TeamDemo.nuspec
+.\nuget.exe push F:\NuGet\TeamCity.$build.number$.nupkg -Source "http://localhost/nuget/packages" -ApiKey API-8KXKBKEZQPTTNJDMQYUAWWBB0VQ
